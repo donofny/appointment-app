d7476f9a838ad21eea442461483ef6444a69b738
Daniel Dabrowski <ddbllr15@gmail.com>
2022-08-02 12:33:41 -0400

    date picket alert change

--- Client/src/app/components/add-appointment/add-appointment.component.ts
+++ Client/src/app/components/add-appointment/add-appointment.component.ts
@@ -36,6 +36,10 @@ export class AddAppointmentComponent implements OnInit {
       alert('Input invalid');
       return;
     }
+    if(Object.prototype.toString.call(this.start) !== "[object Date]"){
+      alert('Please enter a valid date');
+      return;
+    }
     const model = {
       id: this.id,
       name: this.name,
